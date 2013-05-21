5d7391a590cc773356ca2c63bfd2d6c6f5619722
meh <meh@schizofreni.co>
2013-05-21 03:03:32 +0200

    dsl: make keywords work decently without losing spaces

--- src/Crepuscolo/DSL.hs
+++ src/Crepuscolo/DSL.hs
@@ -6,10 +6,12 @@ module Crepuscolo.DSL
 
 import Crepuscolo
 
+import Text.Regex.PCRE ((=~))
+
 (|||) :: Highlighter -> Highlighter -> Highlighter
 a ||| b = b . a
 
-region :: String -> String -> String
+region :: String -> String -> String -> Highlighter
 region group start end =
     undefined
 
@@ -17,15 +19,19 @@ match :: String -> String -> Highlighter
 match group regex =
     undefined
 
-keyword :: String -> [String] -> (String -> Bool) -> Highlighter
-keyword group names isKeyword = \h -> make h
+keyword :: String -> [String] -> Highlighter
+keyword group names = \h -> make h
   where
     make (Group elements) =
         Group $ map make elements
 
-    make (Content string) = Group $ map (\w ->
-        if isKeyword w && w `elem` names
+    make (Content string) = Group $ map keyword keywords
+      where
+        keyword w = if w `elem` names
            then Highlight Keyword group (Content w)
-           else Content w) (words string)
+           else Content w
+
+        keywords = concat $ map (filter (not . null) . tail)
+                                (string =~ "(\\s+)?([^\\s]+)?(\\s+)?" :: [[String]])
 
     make x = x
