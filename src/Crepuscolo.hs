0d967a80498e49b81c2dac59c207b691810f877e
meh <meh@schizofreni.co>
2013-05-21 03:04:02 +0200

    crepuscolo: add flattening

--- src/Crepuscolo.hs
+++ src/Crepuscolo.hs
@@ -7,7 +7,9 @@ module Crepuscolo
     , dehighlight
     ) where
 
-data Context = Keyword | Match | Region | Cluster deriving (Show)
+import Data.List (groupBy)
+
+data Context = Keyword | Match | Region | Cluster deriving (Show, Eq)
 
 data Highlighted = Content String
                  | Group [Highlighted]
@@ -15,13 +17,29 @@ data Highlighted = Content String
                              , name    :: String
                              , content :: Highlighted
                              }
-                 deriving (Show)
+                 deriving (Show, Eq)
 
 type Highlighter = Highlighted -> Highlighted
 
 highlight :: Highlighter -> String -> Highlighted
 highlight h s =
     flatten $ h $ Content s
+  where
+    flatten (Content s) = Content s
+    flatten h@(Highlight { content = c }) = h { content = flatten c }
+    flatten (Group c) = case map flatten c of
+        (x:[])  -> x
+        c'@(_:_) -> Group $ merge $ contents c'
+      where 
+        isContent (Content _) = True
+        isContent _           = False
+
+        contents = groupBy $ \a b ->
+            isContent a && isContent b
+
+        merge cs = concat $ flip map cs $ \xs@(x:_) -> case x of
+            Content _ -> [Content $ concat $ map (\(Content s) -> s) xs]
+            _         -> xs
 
 extend h s =
     h s
@@ -30,10 +48,3 @@ dehighlight :: Highlighted -> String
 dehighlight (Content s) = s
 dehighlight (Highlight { content = c }) = dehighlight c
 dehighlight (Group c) = concat $ map dehighlight c
-
-flatten :: Highlighted -> Highlighted
-flatten (Content s) = Content s
-flatten h@(Highlight { content = c }) = h { content = flatten c }
-flatten (Group c) = case map flatten c of
-    (x:[])  -> x
-    c@(_:_) -> Group c
