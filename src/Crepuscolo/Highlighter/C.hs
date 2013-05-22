e1d03e18bfc2b8c27aad56ea5a3ec0e37206d67e
meh <meh@schizofreni.co>
2013-05-22 10:07:22 +0200

    highlighter/c: use the new DSL

--- src/Crepuscolo/Highlighter/C.hs
+++ src/Crepuscolo/Highlighter/C.hs
@@ -3,10 +3,16 @@ module Crepuscolo.Highlighter.C
     ) where
 
 import Crepuscolo
-import Crepuscolo.DSL
+import Crepuscolo.DSL hiding (keyword)
+import qualified Crepuscolo.DSL as DSL (keyword)
+
+import Text.Regex.PCRE
 
 highlighter :: Highlighted -> Highlighted
 highlighter = keyword "c:statement" ["goto", "break", "return", "continue", "asm"]
           ||| keyword "c:label" ["case", "default"]
           ||| keyword "c:conditional" ["if", "else", "switch"]
           ||| keyword "c:repeat" ["while", "for", "do"]
+
+keyword group names =
+    DSL.keyword group names "[A-Za-z_$][A-Za-z0-9_$]+"
