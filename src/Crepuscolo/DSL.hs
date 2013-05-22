f721373c0948621c10e92d17c89b01f0e394ce35
meh <meh@schizofreni.co>
2013-05-22 10:07:03 +0200

    dsl: properly split keywords from operators/punctuation

--- src/Crepuscolo/DSL.hs
+++ src/Crepuscolo/DSL.hs
@@ -6,7 +6,7 @@ module Crepuscolo.DSL
 
 import Crepuscolo
 
-import Text.Regex.PCRE ((=~))
+import Text.Regex.PCRE ((=~), getAllMatches)
 
 (|||) :: Highlighter -> Highlighter -> Highlighter
 a ||| b = b . a
@@ -19,19 +19,36 @@ match :: String -> String -> Highlighter
 match group regex =
     undefined
 
-keyword :: String -> [String] -> Highlighter
-keyword group names = \h -> make h
+keyword :: String -> [String] -> String -> Highlighter
+keyword group names keywordRegex = \h -> make h
   where
     make (Group elements) =
         Group $ map make elements
 
-    make (Content string) = Group $ map keyword keywords
+    make (Content string) = Group $ map keyword pieces
       where
         keyword w = if w `elem` names
            then Highlight Keyword group (Content w)
            else Content w
 
-        keywords = concat $ map (filter (not . null) . tail)
-                                (string =~ "(\\s+)?([^\\s]+)?(\\s+)?" :: [[String]])
+        pieces = concat $ flip map spaced $ \s ->
+            if s =~ "\\s"
+               then [s]
+               else split s (getAllMatches (s =~ keywordRegex) :: [(Int, Int)])
+          where
+            split string matches = get 0 string matches
+              where get _ "" []                                 = []
+                    get _ string []                             = [string]
+                    get current string ((0, length) : next) =
+                        let (this, tail)  = splitAt length string
+                            in this : get length tail next
+                    get current string ((start, length) : next) =
+                        let (before, tail) = splitAt (start - current) string
+                            (this, tail')  = splitAt length tail
+                            in before : this : get (start + length) tail' next
+
+            spaced = concat $
+                map (filter (not . null) . tail)
+                    (string =~ "(\\s+)?([^\\s]+)?(\\s+)?" :: [[String]])
 
     make x = x
